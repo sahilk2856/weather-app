719c29a24c7b0763a0248b9351044156b273ba99
sahilk2856 <sahil.k@antino.io>
2023-02-14 17:07:10 +0530

    new commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoiam5ka2puZiIsImEiOiJjbGR5NW85MjUwM3I5M3ZvMGFienAweDl0In0.jF7u95SX5kKyoOtkVSmuJA&limit=1'
+
+    request({ url:url, json: true }, (error,response) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
